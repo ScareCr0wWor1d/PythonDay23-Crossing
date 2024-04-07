d45700669ab7b84b39b5add6e269bc609d1756eb
ScareCr0wWor1d <proulx_marc@hotmail.com>
2024-04-07 17:39:18 -0400

    display correction

--- scoreboard.py
+++ scoreboard.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Turtle
 
 FONT = ("Courier", 24, "normal")
@@ -9,16 +10,31 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color('blue')
         self.hideturtle()
-        self.penup()
-        self.goto(-200, 260)
         self.updatesb(1)
 
+
     def updatesb(self, niv):
+        self.penup()
+        self.goto(-200, 260)
         self.clear()
         self.write(f"Level: {niv}", font=FONT, align='center')
+        self.drawboard()
 
     def game_over(self):
+        self.penup()
         self.clear()
         self.goto(0, 0)
         self.color('red')
         self.write("GAME OVER", font=FONTGO, align='center')
+
+    def drawboard(self):
+        self.goto(-300, -260)
+        self.color('white')
+        self.pendown()
+        self.goto(300, -260)
+        self.penup()
+        self.goto(-300, 260)
+        self.pendown()
+        self.color('white')
+        self.pendown()
+        self.goto(300, 260)
